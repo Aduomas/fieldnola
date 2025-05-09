f934c8b440d8245faa66f885133b48df5250bc25
Adomas Valiukevicius <adomasval04@gmail.com>
2025-05-09 21:12:28 +0300

    Initial Commit

---
+++ fieldnola/fieldnolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  fieldnolaApp.swift
+//  fieldnola
+//
+//  Created by Adomas Valiukevicius on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct fieldnolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fieldnolaTests/fieldnolaTests.swift
@@ -0,0 +1,17 @@
+//
+//  fieldnolaTests.swift
+//  fieldnolaTests
+//
+//  Created by Adomas Valiukevicius on 5/9/25.
+//
+
+import Testing
+@testable import fieldnola
+
+struct fieldnolaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
